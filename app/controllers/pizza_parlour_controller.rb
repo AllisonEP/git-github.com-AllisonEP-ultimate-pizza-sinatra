27a747ea27de6bd8cd4a959b05549b353609c588
Allison Perry <allisonpshortnorth@gmail.com>
2021-01-15 13:21:18 -0500

    'added pizza parlour create'

--- app/controllers/pizza_parlour_controller.rb
+++ app/controllers/pizza_parlour_controller.rb
@@ -6,6 +6,31 @@ class PizzaParloursController < ApplicationController
         erb :'pizza-parlours/index'
     end 
 
+    get '/pizza-parlours/new' do
+        erb :'pizza-parlours/new'
+    end
 
+    post '/pizza-parlours' do
+        pizza_parlour = current_user.pizza_parlours.build(params[:pizza_parlour])
+
+        if pizza_parlour.save
+            redirect '/pizza-parlours'
+        else
+            redirect '/pizza-parlours/new'
+        end
+    end
+
+    get '/pizza-parlours/:id' do
+        find_pizza_parlour
+        redirect_if_not_exit
+        erb :'pizza_parlours/show'
+    end
+
+    private 
+
+    def find_pizza_parlour
+        @pizza_parlour = PizzaParlour.find_by_id(params[:id])
+    end
+    
 
 end
